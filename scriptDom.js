9f1d4ffa30815d9b2beb806450525b3ea5f55048
manuelbustillos <bustillossmanuel@gmail.com>
2024-03-21 02:50:07 -0300

    actualizando ejercicio

---
+++ scriptDom.js
@@ -0,0 +1,24 @@
+const contenedor = document.querySelector(".flex-container");
+
+function crearLlave(nombre,modelo,precio){
+    img = "<img src=`llave.png`>";
+    nombre = `<h2>${nombre}</h2>`;
+    modelo = `<h3>${modelo}</h3>`;
+    precio = `<p><b>$${precio}</b></p>`;
+    return [img,nombre,modelo,precio];
+}
+
+let fragmento = document.createDocumentFragment();
+
+
+for(var i =0; i<20; i++){
+    let modeloRandom = Math.random()*10000;
+    let precioRandom = Math.random()*10+30;
+    let llave = crearLlave(`llave ${i}`,`modelo ${modeloRandom}`,precioRandom);
+    let div = document.createElement("DIV");
+    div.classList.add(`item-${i}`,`flex-item`);
+    div.innerHTML = llave[0] + llave[1] + llave[2] +llave[3];
+    fragmento.appendChild(div);
+}
+
+contenedor.appendChild(fragmento);
